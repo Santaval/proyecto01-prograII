5b582853a800a986c16ce2090714876803572b76
Aaron Santana <aaron.satana@ucr.ac.cr>
2023-05-01 13:27:31 -0600

    matrix read errors managment

--- src/Solution.c
+++ src/Solution.c
@@ -2,28 +2,47 @@
 #include <stdio.h>
 #include <Solution.h>
 
+enum EXIT_CODES {ERR, SUCCESS, INSUFICIENT_DATA, INVALID_DATA};
+
 int main() {
 
     /// Creating int variables to keep row, cols values and array for cantinious matrix in dynamic memory
-    int terrainRows = 0;
-    int terrainCols = 0;
-    char* terrainBoard;
+    int terrainRows = 0; // matrix rows
+    int terrainCols = 0; // matix cols
+    char* terrainBoard; // pointer to char array in dynamic memory
 
+    /// reading matrix dimmensions and validate them
     if (scanf("%d %d", &terrainRows, &terrainCols) == 2) {
+        /// assign dynamic memory spae to keep continious matix
         terrainBoard = (char*) malloc((sizeof(char) * (terrainCols * terrainRows)) + sizeof(char));
-        readMatrix(terrainBoard, terrainRows, terrainCols);
-        printMatrix(terrainBoard, terrainRows, terrainCols);
-        free(terrainBoard);
+        const int readMatixReturn = readMatrix(terrainBoard, terrainRows, terrainCols);
+        if ( readMatixReturn == SUCCESS) {
+            printMatrix(terrainBoard, terrainRows, terrainCols); // printing matix (delete)
+        } else if (readMatixReturn == INVALID_DATA) {
+            puts("Invalid data");
+            return EXIT_FAILURE;
+        } else {
+            puts("Insuficient data");
+            return EXIT_FAILURE;
+        }
+       
+        free(terrainBoard); // free continious matix memory
+    } else {
+        puts("Invalid data");
+        return EXIT_FAILURE;
     }
 
+    return EXIT_SUCCESS;
 }
 
 
-void readMatrix(char* terrainBoard, const int rows, const int cols) {
+int readMatrix(char* terrainBoard, const int rows, const int cols) {
     for(int count = 0; count < rows * cols; count++) {
-        scanf(" %c", &terrainBoard[count]);
+       if ( scanf(" %c", &terrainBoard[count]) != 1) return INSUFICIENT_DATA;
+       if (terrainBoard[count] != 'X' && terrainBoard[count] != '-') return INVALID_DATA;
     }
     terrainBoard[rows * cols] = '\0';
+    return SUCCESS;
 }
 
 void printMatrix(char* terrainBoard, const int rows, const int cols) {
--- src/Solution.h
+++ src/Solution.h
@@ -1,2 +1,2 @@
-void readMatrix(char* terrainBoard, const int rows, const int cols);
+int readMatrix(char* terrainBoard, const int rows, const int cols);
 void printMatrix(char* terrainBoard, const int rows, const int cols);
