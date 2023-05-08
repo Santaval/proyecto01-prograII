f8e8f2106264657e991b83acc38100d9deba5640
FettRon <masxdj7@gmail.com>
2023-05-08 16:59:02 -0600

    documentation

--- src/SearchTerrainAreas.c
+++ src/SearchTerrainAreas.c
@@ -7,44 +7,36 @@
 #include <stdlib.h>
 #include "SearchTerrainAreas.h"
 
-
-
 void SearchingForShelterInTheFlood(char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols ) {
     safeArea_t** safeAreaArr = malloc(sizeof(safeArea_t**) * (terrainRows * terrainCols));
     searchTerrainAreas(terrain, terrainRows, terrainCols);
     printMatrix(terrain, terrainRows, terrainCols);
 }
 
-
-
 void searchTerrainAreas(char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols) {
     char* terrainClone = malloc((sizeof(char) * (terrainCols * terrainRows)) + sizeof(char));
     safeArea_t** safeAreaArr = malloc(sizeof(safeArea_t*) * (terrainRows * terrainCols));
     for (terrainSize_t index = 0; index < terrainRows * terrainCols; index++) {
-        safeAreaArr[index] = NULL;
+        safeAreaArr[index] = NULL; //initialize the matrix
     }
     terrainSize_t pushedAreas = 0;
     cloneTerrain(terrain, terrainClone, terrainRows, terrainCols);
     for (terrainSize_t rowCounter = 0; rowCounter < terrainRows; rowCounter++) {
         for (terrainSize_t colCounter = 0; colCounter < terrainCols; colCounter++) {
             if (terrainClone[terrainCols * rowCounter + colCounter] == '-') {
+                //starts searching for safe areas
                 safeArea_t* newArea = malloc(sizeof(safeArea_t));
-
+                //keeps the coordenates of one cell of the safe area
                 newArea -> row = rowCounter;
                 newArea -> col = colCounter;
-                newArea -> size = 0;
-
+                newArea -> size = 0; //keeps the size of the safe area
                 searchTerrain(terrainClone, newArea, rowCounter, colCounter, terrainRows, terrainCols);
-                safeAreaArr[pushedAreas] = newArea;
-                pushedAreas++;
+                safeAreaArr[pushedAreas] = newArea; //saves the safe area in a array of structs
+                pushedAreas++; //goes for the next safe area
             }
         }
     }
-
     replace(safeAreaArr, terrain, terrainRows, terrainCols);
-
-    // free(safeAreaArr);
-    // free(terrainClone);
 }
 
 void cloneTerrain(char* terrain, char* terrainClone, terrainSize_t terrainRows, terrainSize_t terrainCols) {
@@ -58,8 +50,8 @@ void searchTerrain(char* terrain, safeArea_t* newArea, terrainSize_t cellRow, te
         terrainSize_t terrainRows, terrainSize_t terrainCols) {
     if (terrain[terrainCols * cellRow + cellCol] == '-') {
         terrain[terrainCols * cellRow + cellCol] = 'C';
-        newArea -> size+= 1;
-
+        newArea -> size+= 1; 
+        //increase the size of the safe area and start a recursive search in 4 directions (up, down, right, left)
         if (cellRow + 1 < terrainRows) searchTerrain(terrain, newArea, cellRow + 1, cellCol, terrainRows, terrainCols);
         if (cellRow - 1 >= 0) searchTerrain(terrain, newArea, cellRow - 1, cellCol, terrainRows, terrainCols);
         if (cellCol + 1 < terrainCols) searchTerrain(terrain, newArea, cellRow, cellCol + 1, terrainRows, terrainCols);
@@ -71,6 +63,7 @@ void replaceCell(char* terrain, terrainSize_t row, terrainSize_t col, terrainSiz
         terrainSize_t terrainCols) {
      if (terrain[terrainCols * row + col] == '-') {
         terrain[terrainCols * row + col] = 'R';
+        //start a recursive search in 4 directions (up, down, right, left)
         if (row + 1 < terrainRows) replaceCell(terrain, row + 1, col, terrainRows, terrainCols);
         if (row - 1 >= 0) replaceCell(terrain, row - 1, col, terrainRows, terrainCols);
         if (col + 1 < terrainCols) replaceCell(terrain, row, col + 1, terrainRows, terrainCols);
@@ -79,26 +72,26 @@ void replaceCell(char* terrain, terrainSize_t row, terrainSize_t col, terrainSiz
 }
 
 void replace(safeArea_t** safeAreaArr, char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols) {
-    terrainSize_t arrLength = terrainRows * terrainCols;
-    terrainSize_t biggest = 0;
-    int areas = 0;
+    terrainSize_t arrLength = terrainRows * terrainCols; //quantitie of elements of the array
+    terrainSize_t biggest = 0; //keeps the size of the biggest safe area
+    int areas = 0; //areas that have been succesfully replaced
     for (terrainSize_t i = 0; i < arrLength; i++) {
         if (safeAreaArr[i] != NULL) {
             if (safeAreaArr[i]->size > biggest) {
-                biggest = safeAreaArr[i]->size;
+                biggest = safeAreaArr[i]->size; //keeps the size of the biggest area
             }
         }
     }
-
-    for (int i = 0; i < arrLength; i++) {
+    //analyzes the entire array, replazing every safe area with a refugee every time the area is the 
+    //same size as the biggest area
+    for (int i = 0; i < arrLength; i++) { 
         if (safeAreaArr[i] != NULL && safeAreaArr[i]->size == biggest) {
             areas++;
             replaceCell(terrain, safeAreaArr[i]->row, safeAreaArr[i]->col, terrainRows, terrainCols);
         }
     }
-    printf("%d\n",areas);
+    printf("%d\n",areas); //prints how many areas have been replaced with refugees
 }
-
 
 void printMatrix(char *terrainBoard, const terrainSize_t rows, const terrainSize_t cols) {
     for (terrainSize_t count = 0; count < rows * cols; count++) {
--- src/SearchTerrainAreas.h
+++ src/SearchTerrainAreas.h
@@ -1,17 +1,84 @@
 typedef long long terrainSize_t;
 
+/**
+ * @struct SafeArea
+ * keeps one coordenate of every safe area and it's size
+ */
 typedef struct SafeArea {
-    terrainSize_t row;
-    terrainSize_t col;
-    terrainSize_t size;
+    terrainSize_t row; /** row of the cell of one safe spot */
+    terrainSize_t col; /** column of the cell of one safe spot */
+    terrainSize_t size; /** size of the safe area */
 } safeArea_t;
 
+/**
+ * @brief Calls the main functions of the program and shows the result.
+ * 
+ * @param terrain continuous matrix of chars that saves the initial terrain.
+ * @param terrainRows rows of the matrix.
+ * @param terrainCols columns of the matrix.
+ */
 void SearchingForShelterInTheFlood(char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols);
+
+/**
+ * @brief search for every safe area in the terrain matrix, saving one 
+ * coordenate of every safe area and its size in an array of safe areas
+ * @param terrain the terrain that it's going to be checked
+ * @param terrainRows total of rows fo the terrain
+ * @param terrainCols total of columns of the terrain
+ */
 void searchTerrainAreas(char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols);
+
+/**
+ * @brief creates a copy of the original matrix, the copy is going to be 
+ * modified later
+ * @param terrain original matrix 
+ * @param terrainClone copied matrix (values are going to be saved here)
+ * @param terrainRows total of rows of both matrixes
+ * @param terrainCols total of columns of both matrixes
+ */
 void cloneTerrain(char* terrain, char* terrainClone, terrainSize_t terrainRows, terrainSize_t terrainCols);
+
+/**
+ * @brief replaces the current '-' cell with a 'C' to remember that it's
+ * already checked, then checks every cell arround the current one and
+ * repeat the same procces
+ * @param terrain copy of the original matrix that is going to be checked
+ * @param newArea struct of the safe area that keeps the size of the area
+ * @param cellRow row of the current cell
+ * @param cellCol column of the current cell
+ * @param terrainRows total of rows of the matrix
+ * @param terrainCols total of columns of the matrix
+ */
 void searchTerrain(char* terrain, safeArea_t* newArea, terrainSize_t cellRow, terrainSize_t cellCol,
     terrainSize_t terrainRows, terrainSize_t terrainCols);
+
+/**
+ * @brief when the biggest safe area of the terrain is already known, every
+ * '-' is replaced with a 'R'
+ * @param terrain original matrix of the terrain
+ * @param row row of the current cell
+ * @param col column of the current cell
+ * @param terrainRows total rows of the matrix
+ * @param terrainCols total columns of the matrix
+ */
 void replaceCell(char* terrain, terrainSize_t row, terrainSize_t col, terrainSize_t terrainRows,
     terrainSize_t terrainCols);
+
+/**
+ * @brief analyzes the array of safe areas in order to finde the biggest(s) one(s)
+ * 
+ * @param safeAreaArr array of structs of every safe area in the terrain
+ * @param terrain original matrix of the terrain
+ * @param terrainRows total of rows of the matrix
+ * @param terrainCols total of columns of the matrix
+ */
 void replace(safeArea_t** safeAreaArr, char* terrain, terrainSize_t terrainRows, terrainSize_t terrainCols);
+
+/**
+ * @brief prints the final result and delete the continuous matrix and the array
+ * 
+ * @param terrainBoard final matrix of the terrain
+ * @param rows total of rows of the matrix
+ * @param cols total of columns of the matrix
+ */
 void printMatrix(char *terrainBoard, const terrainSize_t rows, const terrainSize_t cols);
